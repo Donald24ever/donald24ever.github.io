e4a84ede59d7c72c157d887add50703e5375bf0c
donald24ever <donaldo24ever@gmail.com>
2018-10-26 08:19:33 +0100

    resume changes

---
+++ precache-manifest.1f47115175f5b3d3c2fda6ac0a9ce58a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0247f2a723f1d1743cce",
+    "url": "/static/js/main.0247f2a7.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "0247f2a723f1d1743cce",
+    "url": "/static/css/main.642260a3.chunk.css"
+  },
+  {
+    "revision": "215d416af1735bfe77ac40080b56fd03",
+    "url": "/index.html"
+  }
+];
